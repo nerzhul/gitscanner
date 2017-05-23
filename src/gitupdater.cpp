078f772493564b10a357366436da77466ce3200c
Loïc Blot <lblot@infopro-digital.com>
2017-05-23 12:54:13 +0200

    Prepare git fetch

--- src/gitupdater.cpp
+++ src/gitupdater.cpp
@@ -24,11 +24,89 @@
  */
 
 #include <iostream>
+#include <git2/repository.h>
+#include <git2/remote.h>
 #include "gitupdater.h"
+
+class GitRemote
+{
+public:
+	GitRemote(git_repository *repo, const char *remote_name)
+	{
+		git_remote_lookup(&m_remote, repo, remote_name);
+	}
+
+	~GitRemote()
+	{
+		git_remote_free(m_remote);
+	}
+
+	GitRemote &fetch()
+	{
+		git_fetch_options fetch_options = GIT_FETCH_OPTIONS_INIT;
+		fetch_options.download_tags = GIT_REMOTE_DOWNLOAD_TAGS_ALL;
+		fetch_options.prune = GIT_FETCH_PRUNE;
+//			git_remote_fetch(remote, nullptr, &fetch_options, nullptr);
+		return *this;
+	}
+private:
+	git_remote *m_remote = nullptr;
+};
+
+class GitRepository
+{
+public:
+	GitRepository(const std::string &path)
+	{
+		m_fail = git_repository_open(&m_repo, std::string(path + "/.git").c_str()) != 0;
+		m_remotes.strings = nullptr;
+		m_remotes.count = 0;
+	}
+
+	~GitRepository()
+	{
+		if (!m_fail) {
+			git_repository_free(m_repo);
+		}
+	}
+
+	GitRepository &list_remotes()
+	{
+		if (m_fail) {
+			return *this;
+		}
+
+		git_strarray_free(&m_remotes);
+		git_remote_list(&m_remotes, m_repo);
+		return *this;
+	}
+
+	GitRepository &fetchall()
+	{
+		if (m_fail) {
+			return *this;
+		}
+
+		for (uint8_t i = 0; i < m_remotes.count; i++) {
+			GitRemote(m_repo, m_remotes.strings[i]).fetch();
+		}
+		return *this;
+	}
+
+private:
+	bool m_fail = false;
+	git_repository *m_repo;
+	git_strarray m_remotes;
+};
 
 GitUpdater& GitUpdater::operator<<(const GitScanner &scanner)
 {
 	for (const auto &path: scanner.m_git_repositories) {
-		std::cout << "Scanning " << path << std::endl;
+		std::cout << "Fetching repository path: " << path << std::endl;
+		GitRepository(path)
+				.list_remotes()
+				.fetchall();
 	}
+
+	return *this;
 }
